a18713d53f0b5e3a8e41889a1cf7c198a7363978
CreativeMD <Creative-Multimedia-Design@gmx.de>
2015-11-14 13:29:47 +0100

    Removed Core Mod

--- src/main/java/com/creativemd/opticmanager/OpticManager.java
+++ src/main/java/com/creativemd/opticmanager/OpticManager.java
@@ -10,7 +10,9 @@ import com.google.common.eventbus.Subscribe;
 import cpw.mods.fml.common.DummyModContainer;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.LoadController;
+import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.ModMetadata;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLConstructionEvent;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import net.minecraft.entity.player.EntityPlayer;
@@ -19,41 +21,19 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 
-public class OpticManager extends DummyModContainer{
+@Mod(modid = OpticManager.modid, version = OpticManager.version, name = "OpticManager")
+public class OpticManager{
 	
-	public static final String modid = "opticmanager"; 
-	
-	public OpticManager() {
-		super(new ModMetadata());
-		ModMetadata meta = getMetadata();
-		meta.modId = modid;
-		meta.name = "OpticManager";
-		meta.version = "0.1"; //String.format("%d.%d.%d.%d", majorVersion, minorVersion, revisionVersion, buildVersion);
-		meta.credits = "CreativeMD";
-		meta.authorList = Arrays.asList("CreativeMD");
-		meta.description = "This mod adds the ability to talk with people in a server.";
-		meta.url = "";
-		meta.updateUrl = "";
-		meta.screenshots = new String[0];
-		meta.logoFile = "";
-	}
+	public static final String modid = "opticmanager";
+	public static final String version = "0.1";
 	
 	public static boolean renderNameTag = true;
 	public static float brightness = 1F;
 	public static EntityPlayer.EnumChatVisibility visibilty;
 	
-	//public static String[] settingNames = {"-500%", "-200%", "-100%", "-50%", "-25%", "100%", "150%", "200%", "500%"};
-	//public static float[] settings = {};
-	
-	@Override
-	public boolean registerBus(EventBus bus, LoadController controller) {
-		bus.register(this);
-		return true;
-	}
-	
 	public ModTab tab;
 	
-	@Subscribe
+	@EventHandler
 	public void init(FMLInitializationEvent evt) {
 		
 		if(FMLCommonHandler.instance().getEffectiveSide().isClient())
