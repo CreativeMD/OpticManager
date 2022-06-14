685d03bad12fbd69c59fa912b8a925ade4f1909b
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-06-14 16:08:28 +0200

    Fixed mixin crash

--- src/main/java/team/creative/opticmanager/mixin/EntityRendererMixin.java
+++ src/main/java/team/creative/opticmanager/mixin/EntityRendererMixin.java
@@ -3,9 +3,13 @@ package team.creative.opticmanager.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import team.creative.opticmanager.OpticManager;
@@ -16,7 +20,7 @@ public class EntityRendererMixin {
     @Inject(at = @At("HEAD"),
             method = "renderNameTag(Lnet/minecraft/world/entity/Entity;Lnet/minecraft/network/chat/Component;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
             cancellable = true)
-    public void renderNameTag(Entity entity, CallbackInfoReturnable<Void> callback) {
+    public void renderNameTag(Entity entity, Component component, PoseStack stack, MultiBufferSource buffer, int i, CallbackInfo callback) {
         if (!OpticManager.CONFIG.renderPlayerNameTag && entity instanceof Player)
             callback.cancel();
     }
